9e2ff78d5f22d9c4f1226b6a4428e567d5c0bc89
Madan.patakota <madan.patakota@gmail.com>
2025-09-25 09:24:18 +0530

    compna

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root-madan',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularBasicsApp_Sep';
+  //title = 'AngularBasicsApp_Sep';
 }
