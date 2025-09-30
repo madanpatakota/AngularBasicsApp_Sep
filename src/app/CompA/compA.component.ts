9811286a1b88b41839f40a92497f8237633966d8
Madan.patakota <madan.patakota@gmail.com>
2025-09-30 09:35:32 +0530

    directives

--- src/app/CompA/compA.component.ts
+++ src/app/CompA/compA.component.ts
@@ -15,14 +15,65 @@ export class CompAComponent{
 
 
      isAdmin = false;
-
-     
-     
-     
-     
      btnClick(){
         this.divColor = "red";
         this.fontColor = 'yellow';
      }
+
+
+     colorType = "brown";
+     employeeType = "Bad";
+
+
+      dummyKey = "madan!1234";
+
+      //myPasswordKey = "!223444!!!*88866^&^";
+
+      myPasswordKey = "**************";
+
+
+      RoleID = 10;   //Adminstartor
+
+      myName = "mohan";
+
+
+
+      evtClickMe(element:any , element1:any){
+        console.log(element);
+        console.log(element1);
+      }
+
+
+//                      0        1      2
+Studentnames = ["Ram" , "Sam" , "John"];
+
+
+Students = [
+    {id : 101 , name :"Ram"},
+    {id : 201 , name :"Sam"},
+    {id : 301 , name :"John"}
+];
+
+
+studnetID  = 100;   //Ram
+
+
+      ///list of data each value print 
+
+      //foreach , for 
+
+
+// switch(____)
+//    case
+//    case
+
+
+
+    //  if(true)
+    //  {
+    //          ////////
+
+    //  }
+    //  else {  }
 
 }
--- src/app/comp-b/comp-b.component.ts
+++ src/app/comp-b/comp-b.component.ts
@@ -82,6 +82,7 @@ export class CompBComponent {
 
 
     SubjectName = "Angular";
+    
 
 
 
